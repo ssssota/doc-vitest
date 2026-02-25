1c2a37e87d4868a9bfa56ee2614bf8d454994975
mspiess <30656914+mspiess@users.noreply.github.com>
2026-02-25 21:32:37 +0100

    fix: code generation for markdown file ending on closing code fence

---
+++ packages/vite-plugin-doctest/src/transformers/markdown.spec.ts
@@ -0,0 +1,40 @@
+import { expect, it } from "vitest";
+import { transform } from "./markdown";
+
+it("should generate testcode", async () => {
+	const { code } = await transform(
+		`~~~ts @import.meta.vitest
+expect(1 + 1).toBe(2);
+~~~
+`,
+		"add.ts",
+		{ markdownSetup: "" },
+	);
+
+	expect(code).toMatchInlineSnapshot(`
+    "if (import.meta.vitest) {
+    const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
+    import.meta.vitest.test("add.ts#0", async () => {
+    expect(1 + 1).toBe(2);
+    });
+    }"
+  `);
+});
+
+it("should generate testcode when file ends with closing code fence", async () => {
+	const { code } = await transform(
+		`~~~ts @import.meta.vitest
+expect(1 + 1).toBe(2);
+~~~`,
+		"add.ts",
+		{ markdownSetup: "" },
+	);
+
+	expect(code).toMatchInlineSnapshot(`
+    "if (import.meta.vitest) {
+    const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
+    import.meta.vitest.test("add.ts#0", async () => {
+    expect(1 + 1).toBe(2);
+    });}"
+  `);
+});
