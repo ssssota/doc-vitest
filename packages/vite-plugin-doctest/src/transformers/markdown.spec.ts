9fdbc24ea82db987e8ccc8f460c1fcff65cae353
simonvarey <simonvarey@gmail.com>
2026-03-12 14:41:08 -0400

    fix: Updated tests with new line before final brace in Markdown transform

--- packages/vite-plugin-doctest/src/transformers/markdown.spec.ts
+++ packages/vite-plugin-doctest/src/transformers/markdown.spec.ts
@@ -2,16 +2,36 @@ import { expect, it } from "vitest";
 import { transform } from "./markdown";
 
 it("should generate testcode", async () => {
-	const { code } = await transform(
-		`~~~ts @import.meta.vitest
+  const { code } = await transform(
+    `~~~ts @import.meta.vitest
 expect(1 + 1).toBe(2);
 ~~~
 `,
-		"add.ts",
-		{ markdownSetup: "" },
-	);
+    "add.ts",
+    { markdownSetup: "" },
+  );
 
-	expect(code).toMatchInlineSnapshot(`
+  expect(code).toMatchInlineSnapshot(`
+    "if (import.meta.vitest) {
+    const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
+    import.meta.vitest.test("add.ts#0", async () => {
+    expect(1 + 1).toBe(2);
+    });
+    
+    }"
+  `);
+});
+
+it("should generate testcode when file ends with closing code fence", async () => {
+  const { code } = await transform(
+    `~~~ts @import.meta.vitest
+expect(1 + 1).toBe(2);
+~~~`,
+    "add.ts",
+    { markdownSetup: "" },
+  );
+
+  expect(code).toMatchInlineSnapshot(`
     "if (import.meta.vitest) {
     const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
     import.meta.vitest.test("add.ts#0", async () => {
@@ -21,38 +41,21 @@ expect(1 + 1).toBe(2);
   `);
 });
 
-it("should generate testcode when file ends with closing code fence", async () => {
-	const { code } = await transform(
-		`~~~ts @import.meta.vitest
-expect(1 + 1).toBe(2);
-~~~`,
-		"add.ts",
-		{ markdownSetup: "" },
-	);
-
-	expect(code).toMatchInlineSnapshot(`
-    "if (import.meta.vitest) {
-    const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
-    import.meta.vitest.test("add.ts#0", async () => {
-    expect(1 + 1).toBe(2);
-    });}"
-  `);
-});
-
 it("should not include @import.meta.vitest in filename", async () => {
-	const { code } = await transform(
-		`~~~ts:add.ts@import.meta.vitest
+  const { code } = await transform(
+    `~~~ts:add.ts@import.meta.vitest
 expect(1 + 1).toBe(2);
 ~~~`,
-		"add.ts",
-		{ markdownSetup: "" },
-	);
+    "add.ts",
+    { markdownSetup: "" },
+  );
 
-	expect(code).toMatchInlineSnapshot(`
+  expect(code).toMatchInlineSnapshot(`
     "if (import.meta.vitest) {
     const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
     import.meta.vitest.test("add.ts", async () => {
     expect(1 + 1).toBe(2);
-    });}"
+    });
+    }"
   `);
 });
