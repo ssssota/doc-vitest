48dec2ba33d89e700839cb5c48baa520740e8901
ssssota <sotaro.tomikawa@zozo.com>
2026-03-13 11:26:13 +0900

    chore: fmt + changeset

---
+++ .changeset/early-chicken-cheer.md
@@ -0,0 +1,5 @@
+---
+"vite-plugin-doctest": patch
+---
+
+Fix markdown transformation with extra lines
--- packages/vite-plugin-doctest/src/transformers/markdown.spec.ts
+++ packages/vite-plugin-doctest/src/transformers/markdown.spec.ts
@@ -2,16 +2,16 @@ import { expect, it } from "vitest";
 import { transform } from "./markdown";
 
 it("should generate testcode", async () => {
-  const { code } = await transform(
-    `~~~ts @import.meta.vitest
+	const { code } = await transform(
+		`~~~ts @import.meta.vitest
 expect(1 + 1).toBe(2);
 ~~~
 `,
-    "add.ts",
-    { markdownSetup: "" },
-  );
+		"add.ts",
+		{ markdownSetup: "" },
+	);
 
-  expect(code).toMatchInlineSnapshot(`
+	expect(code).toMatchInlineSnapshot(`
     "if (import.meta.vitest) {
     const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
     import.meta.vitest.test("add.ts#0", async () => {
@@ -23,15 +23,15 @@ expect(1 + 1).toBe(2);
 });
 
 it("should generate testcode when file ends with closing code fence", async () => {
-  const { code } = await transform(
-    `~~~ts @import.meta.vitest
+	const { code } = await transform(
+		`~~~ts @import.meta.vitest
 expect(1 + 1).toBe(2);
 ~~~`,
-    "add.ts",
-    { markdownSetup: "" },
-  );
+		"add.ts",
+		{ markdownSetup: "" },
+	);
 
-  expect(code).toMatchInlineSnapshot(`
+	expect(code).toMatchInlineSnapshot(`
     "if (import.meta.vitest) {
     const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
     import.meta.vitest.test("add.ts#0", async () => {
@@ -42,15 +42,15 @@ expect(1 + 1).toBe(2);
 });
 
 it("should not include @import.meta.vitest in filename", async () => {
-  const { code } = await transform(
-    `~~~ts:add.ts@import.meta.vitest
+	const { code } = await transform(
+		`~~~ts:add.ts@import.meta.vitest
 expect(1 + 1).toBe(2);
 ~~~`,
-    "add.ts",
-    { markdownSetup: "" },
-  );
+		"add.ts",
+		{ markdownSetup: "" },
+	);
 
-  expect(code).toMatchInlineSnapshot(`
+	expect(code).toMatchInlineSnapshot(`
     "if (import.meta.vitest) {
     const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
     import.meta.vitest.test("add.ts", async () => {
@@ -61,16 +61,16 @@ expect(1 + 1).toBe(2);
 });
 
 it("should generate testcode with text on final line", async () => {
-  const { code } = await transform(
-    `~~~ts @import.meta.vitest
+	const { code } = await transform(
+		`~~~ts @import.meta.vitest
 expect(1 + 1).toBe(2);
 ~~~
 Extra text`,
-    "add.ts",
-    { markdownSetup: "" },
-  );
+		"add.ts",
+		{ markdownSetup: "" },
+	);
 
-  expect(code).toMatchInlineSnapshot(`
+	expect(code).toMatchInlineSnapshot(`
     "if (import.meta.vitest) {
     const {assert,chai,createExpect,expect,getRunningMode,isWatchMode,should,vi,vitest} = import.meta.vitest;
     import.meta.vitest.test("add.ts#0", async () => {
